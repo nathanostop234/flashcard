2a8ce1a117693f10f7cad4b2584033c3a5676a50
nathanostop234 <163007786+nathanostop234@users.noreply.github.com>
2024-10-07 11:15:52 -0300

    .

---
+++ pergunta.js
@@ -0,0 +1,30 @@
+criaCartao(
+    'Programação 2.0'
+    'O que é phyton?'
+    'O phyton é uma linguagem de programação'
+)
+criaCartao(
+    'Gordura'
+    'Porque o Nathan é gordo?'
+    'Porque ele come muito igual baleia'
+)
+criaCartao(
+    'Leo noob'
+    'Porque o Leo é noob?'
+    'Porque ele odeia preto'
+)
+criaCartao(
+    'Bernardo Gay'
+    'Porque o Bernardo é Homosexual?'
+    'Porque ele gosta de dar o cu'
+)
+criaCartao(
+    'Rafaeul'
+    'O Rafaeul é forçado?'
+    'Sim! Ele é homosexual forçado'
+)
+criaCartao(
+    'Atentado'
+    'Dia 11/09/2001 as 9:14 foi um dia que deveria ter feriado?'
+    'Sim!!! O melhor dia na opnião de todos'
+)
