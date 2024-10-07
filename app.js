705f7a1be81f947cefe9fe858b9e74c27bb219e7
nathanostop234 <163007786+nathanostop234@users.noreply.github.com>
2024-10-07 11:33:01 -0300

    ........

--- app.js
+++ app.js
@@ -3,3 +3,14 @@ function criaCartao(categoria, pergunta, resposta) {
     let cartao = document.getElementById('article');
     cartao.className = 'cartao';
 }
+
+cartao.innerHTML =
+<div class="cartao__conteudo">
+                <h3>Programação</h3>
+                <div class="cartao__conteudo__pergunta">
+                    O que é JavaScript?                
+                </div>
+                <div class="cartao__conteudo_resposta">
+                    <p>O JavaScript é uma linguaguem de programação</p>
+                </div>
+                </div>
