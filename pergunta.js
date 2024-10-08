1565449d1e45cfdb98e8c04134a186ac9c1917f8
nathanostop234 <163007786+nathanostop234@users.noreply.github.com>
2024-10-08 08:47:46 -0300

    .......

--- pergunta.js
+++ pergunta.js
@@ -1,30 +1,30 @@
 criaCartao(
-    'Programação 2.0'
-    'O que é phyton?'
+    'Programação 2.0',
+    'O que é phyton?',
     'O phyton é uma linguagem de programação'
 )
 criaCartao(
-    'Gordura'
-    'Porque o Nathan é gordo?'
+    'Gordura',
+    'Porque o Nathan é gordo?',
     'Porque ele come muito igual baleia'
 )
 criaCartao(
-    'Leo noob'
-    'Porque o Leo é noob?'
+    'Leo noob',
+    'Porque o Leo é noob?',
     'Porque ele odeia preto'
 )
 criaCartao(
-    'Bernardo Gay'
-    'Porque o Bernardo é Homosexual?'
+    'Bernardo Gay',
+    'Porque o Bernardo é Homosexual?',
     'Porque ele gosta de dar o cu'
 )
 criaCartao(
-    'Rafaeul'
-    'O Rafaeul é forçado?'
+    'Rafaeul',
+    'O Rafaeul é forçado?',
     'Sim! Ele é homosexual forçado'
 )
 criaCartao(
-    'Atentado'
-    'Dia 11/09/2001 as 9:14 foi um dia que deveria ter feriado?'
+    'Atentado',
+    'Dia 11/09/2001 as 9:14 foi um dia que deveria ter feriado?',
     'Sim!!! O melhor dia na opnião de todos'
 )
