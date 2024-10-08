67e5d6bd6e37d63928c9c73f5e9c2ac909b977a9
nathanostop234 <163007786+nathanostop234@users.noreply.github.com>
2024-10-08 08:39:13 -0300

    ...

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function criaCartao(categoria, pergunta, resposta) {
+function criaCartao(categoria, pergunta, resposta) 
     let container = document.getElementById('container');
     let cartao = document.getElementById('article');
     cartao.className = 'cartao';
