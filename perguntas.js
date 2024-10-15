fe8809f4532096bbeabf98667238827d2aa3b425
nathanostop234 <163007786+nathanostop234@users.noreply.github.com>
2024-10-15 08:35:56 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -3,26 +3,31 @@ CriaCartao(
     'O que é Douglas',
     'O Douglas é uma linguagem de gorduração'
 )
+
 CriaCartao(
     'Gordura',
     'Porque o Nathan é gordo?',
     'Porque ele come muito igual baleia'
 )
+
 CriaCartao(
     'Leo noob',
     'Porque o Leo é noob?',
     'Porque ele odeia preto'
 )
+
 CriaCartao(
     'Bernardo Gay',
     'Porque o Bernardo é Homosexual?',
     'Porque ele gosta de dar o cu'
 )
+
 CriaCartao(
     'Rafaeul',
     'O Rafaeul é forçado?',
     'Sim! Ele é homosexual forçado'
 )
+
 CriaCartao(
     'Atentado',
     'Dia 11/09/2001 as 9:14 foi um dia que deveria ter feriado?',
